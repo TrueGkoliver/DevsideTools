5f40d020954dd9a0da2e3ea6ce5bdd26c5cd23d8
Gkoliver <37561701+TrueGkoliver@users.noreply.github.com>
2020-06-13 09:59:04 -0500

    Extra localization

--- src/main/java/com/gkoliver/devsidetools/commands/PlayerHealCommands.java
+++ src/main/java/com/gkoliver/devsidetools/commands/PlayerHealCommands.java
@@ -10,6 +10,7 @@ import net.minecraft.command.Commands;
 import net.minecraft.command.arguments.EntityArgument;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.util.text.TranslationTextComponent;
 
 public class PlayerHealCommands {
 	public static class HealCommand {
@@ -32,14 +33,15 @@ public class PlayerHealCommands {
 		public static void register(CommandDispatcher<CommandSource> dispatcher) {
 			dispatcher.register(Commands.literal("saturation")
 					.then(Commands.argument("targets", EntityArgument.players()).executes((cmd)->{
-						return heal(20, EntityArgument.getPlayers(cmd, "targets"));
+						return heal(cmd.getSource(), 20, EntityArgument.getPlayers(cmd, "targets"));
 					}).then(Commands.argument("amt", IntegerArgumentType.integer(0, 10000)).executes((cmd)->{
-						return heal(IntegerArgumentType.getInteger(cmd, "amt"), EntityArgument.getPlayers(cmd, "targets"));
+						return heal(cmd.getSource(), IntegerArgumentType.getInteger(cmd, "amt"), EntityArgument.getPlayers(cmd, "targets"));
 					}))));
 		}
-		public static int heal(int amt, Collection<ServerPlayerEntity> targets) {
+		public static int heal(CommandSource source, int amt, Collection<ServerPlayerEntity> targets) {
 			for (PlayerEntity player : targets) {
 				player.getFoodStats().setFoodSaturationLevel(amt);
+				source.sendFeedback(new TranslationTextComponent("commands.saturate"), true);
 			}
 			return 1;
 		}
@@ -49,14 +51,15 @@ public class PlayerHealCommands {
 		public static void register(CommandDispatcher<CommandSource> dispatcher) {
 			dispatcher.register(Commands.literal("hunger")
 					.then(Commands.argument("targets", EntityArgument.players()).executes((cmd)->{
-						return heal(20, EntityArgument.getPlayers(cmd, "targets"));
+						return heal(cmd.getSource(), 20, EntityArgument.getPlayers(cmd, "targets"));
 					}).then(Commands.argument("amt", IntegerArgumentType.integer(0, 10000)).executes((cmd)->{
-						return heal(IntegerArgumentType.getInteger(cmd, "amt"), EntityArgument.getPlayers(cmd, "targets"));
+						return heal(cmd.getSource(), IntegerArgumentType.getInteger(cmd, "amt"), EntityArgument.getPlayers(cmd, "targets"));
 					}))));
 		}
-		public static int heal(int amt, Collection<ServerPlayerEntity> targets) {
+		public static int heal(CommandSource source, int amt, Collection<ServerPlayerEntity> targets) {
 			for (PlayerEntity player : targets) {
 				player.getFoodStats().setFoodLevel(amt);
+				source.sendFeedback(new TranslationTextComponent("commands.hunger"), true);
 			}
 			return 1;
 		}
