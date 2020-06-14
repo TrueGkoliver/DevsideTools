3b9f9db1081b1eb1070c92a7bf9f6b371a66d733
Gkoliver <37561701+TrueGkoliver@users.noreply.github.com>
2020-06-14 07:23:01 -0500

    Arctic foxes

--- src/main/java/com/gkoliver/devsidetools/core/registry/DevsideToolsItems.java
+++ src/main/java/com/gkoliver/devsidetools/core/registry/DevsideToolsItems.java
@@ -41,6 +41,8 @@ public class DevsideToolsItems {
 	
 	public static final CompoundNBT BROWN_PANDA = new CompoundNBT();
 	public static final CompoundNBT BROWN_MOOSHROOM = new CompoundNBT();
+	public static final CompoundNBT ARCTIC_FOX = new CompoundNBT();
+	
 	public static final HashMap<Integer, CompoundNBT> SLC = new HashMap<Integer,CompoundNBT>();
 	static {
 		registerNBTs();
@@ -64,9 +66,12 @@ public class DevsideToolsItems {
 	
 	
 	
-	
+	public static final Item.Properties SPAWN_EGG_PROPS = new Item.Properties().group(ItemGroup.SEARCH); 
 	public static final RegistryObject<Item> BROWN_MOOSHROOM_SPAWN_EGG = ITEMS.register("brown_mooshroom_spawn_egg", ()->new ItemNBTSpawnEgg(EntityType.MOOSHROOM, 0xBABABA, 0xE520DE, new Item.Properties().group(ItemGroup.SEARCH), BROWN_MOOSHROOM));
 	public static final RegistryObject<Item> BROWN_PANDA_SPAWN_EGG = ITEMS.register("brown_panda_spawn_egg", ()->new ItemNBTSpawnEgg(EntityType.PANDA, 0xA5A5A4, 0x523D30, new Item.Properties().group(ItemGroup.SEARCH), BROWN_PANDA));
+	public static final RegistryObject<Item> ARCTIC_FOX_SPAWN_EGG = ITEMS.register("arctic_fox_spawn_egg", ()->new ItemNBTSpawnEgg(EntityType.FOX, 0, 0, SPAWN_EGG_PROPS, ARCTIC_FOX));
+	
+	
 	
 	//Slabfish
 	public static final RegistryObject<ItemNBTSpawnEgg> SLABFISH_0 = genSlabEgg(0, 0x42662D, 0x25512C);
@@ -80,7 +85,6 @@ public class DevsideToolsItems {
 	public static final RegistryObject<ItemNBTSpawnEgg> SLABFISH_8 = genSlabEgg(8, 0x6E402F, 0x39211A);
 	public static final RegistryObject<ItemNBTSpawnEgg> SLABFISH_9 = genSlabEgg(9, 0x2D4D5B, 0x775837);
 	public static final RegistryObject<ItemNBTSpawnEgg> SLABFISH_10 = genSlabEgg(10, 0xFFFA97, 0x2FA55B);
-	
 	public static final RegistryObject<ItemNBTSpawnEgg> SLABFISH_11 = genSlabEgg(11, 0x37634B, 0x735535);
 	public static final RegistryObject<ItemNBTSpawnEgg> SLABFISH_12 = genSlabEgg(12, 0x3C5E29, 0x4A4030);
 	public static final RegistryObject<ItemNBTSpawnEgg> SLABFISH_13 = genSlabEgg(13, 0x3C5D29, 0x283E1C);
@@ -90,7 +94,6 @@ public class DevsideToolsItems {
 	public static final RegistryObject<ItemNBTSpawnEgg> SLABFISH_17 = genSlabEgg(17, 0x8D8B39, 0x806232);
 	public static final RegistryObject<ItemNBTSpawnEgg> SLABFISH_18 = genSlabEgg(18, 0x4F5A26, 0x614258);
 	public static final RegistryObject<ItemNBTSpawnEgg> SLABFISH_19 = genSlabEgg(19, 0x423B22, 0x2A2815);
-	
 	public static final RegistryObject<ItemNBTSpawnEgg> SLABFISH_20 = genSlabEgg(20, 0x29350E, 0x5A693E);
 	public static final RegistryObject<ItemNBTSpawnEgg> SLABFISH_21 = genSlabEgg(21, 0x769485, 0x617F80);
 	public static final RegistryObject<ItemNBTSpawnEgg> SLABFISH_22 = genSlabEgg(22, 0x757F7F, 0x212E2E);
@@ -100,12 +103,10 @@ public class DevsideToolsItems {
 	public static final RegistryObject<ItemNBTSpawnEgg> SLABFISH_26 = genSlabEgg(26, 0x807F7B, 0x515557);
 	/*public static final RegistryObject<ItemNBTSpawnEgg> SLABFISH_27 = genSlabEgg(27, 0, 0);
 	public static final RegistryObject<ItemNBTSpawnEgg> SLABFISH_28 = genSlabEgg(28, 0, 0);*/
-	
 	public static final RegistryObject<ItemNBTSpawnEgg> SLABFISH_29 = genSlabEgg(29, 0x631A1A, 0x2C0909);
 	//public static final RegistryObject<ItemNBTSpawnEgg> SLABFISH_30 = genSlabEgg(30, 0, 0);
 	public static final RegistryObject<ItemNBTSpawnEgg> SLABFISH_31 = genSlabEgg(31, 0x387A5F, 0x195200);
 	public static final RegistryObject<ItemNBTSpawnEgg> SLABFISH_32 = genSlabEgg(32, 0x2D4A57, 0x51869C);
-	
 	public static final RegistryObject<ItemNBTSpawnEgg> SLABFISH_33 = genSlabEgg(33, 0x2C4B64, 0x844638);
 	public static final RegistryObject<ItemNBTSpawnEgg> SLABFISH_34 = genSlabEgg(34, 0x39684F, 0x28463C);
 	public static final RegistryObject<ItemNBTSpawnEgg> SLABFISH_35 = genSlabEgg(35, 0x9F3131, 0x9FA6AD);
@@ -121,6 +122,7 @@ public class DevsideToolsItems {
 		BROWN_MOOSHROOM.putString("Type", "brown");
 		BROWN_PANDA.putString("MainGene", "brown");
 		BROWN_PANDA.putString("HiddenGene", "brown");
+		ARCTIC_FOX.putString("Type", "snow");
 		for (int i=0; i<43; i++) {
 			System.out.println(i);
 			CompoundNBT compound = new CompoundNBT();
