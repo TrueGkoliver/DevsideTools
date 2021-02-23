8dfa5aaafd43c8fec96e7f05bedb5fbb3ed6da8c
Gkoliver <37561701+TrueGkoliver@users.noreply.github.com>
2021-02-23 11:20:15 -0600

    Panorama command now hides the chat
    pain

--- src/main/java/com/gkoliver/devsidetools/common/commands/PanoramaCommand.java
+++ src/main/java/com/gkoliver/devsidetools/common/commands/PanoramaCommand.java
@@ -1,4 +1,16 @@
 package com.gkoliver.devsidetools.common.commands;
 
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.client.Minecraft;
+import net.minecraft.command.CommandSource;
+import net.minecraft.entity.player.PlayerEntity;
+
 public class PanoramaCommand extends Command {
+    @Override
+    public void register(CommandDispatcher<CommandSource> dispatcher) {
+
+    }
+    public void makePanormaScrenshots(PlayerEntity playerIn) {
+        Minecraft.getInstance().gameSettings.chatVisibility = ChatVisibility.HIDDEN;
+    }
 }
